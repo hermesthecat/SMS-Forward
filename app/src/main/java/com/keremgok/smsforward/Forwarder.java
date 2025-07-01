f78ab9166867754d7cfb5fa64aac15ff682008ca
hermesthecat <4947845+hermesthecat@users.noreply.github.com>
2025-07-01 11:26:24 +0300

    i fucked up! revert to 1.14.0

--- app/src/main/java/com/keremgok/smsforward/Forwarder.java
+++
@@ -1,7 +0,0 @@
-package com.keremgok.smsforward;
-
-public interface Forwarder {
-    void forward(String fromNumber, String content) throws Exception;
-
-    void forward(String fromNumber, String content, long timestamp) throws Exception;
-}
